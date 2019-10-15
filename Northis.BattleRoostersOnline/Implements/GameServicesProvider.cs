028ff38dd7103d77c8f3ddcdad8e6886009f5e09
Andrey Buturlakin <aa.buturlakin@northis.ru>
2019-10-15 15:53:46 +0300

    Добавлен проект для тестирования сервисов

--- Northis.BattleRoostersOnline/Implements/GameServicesProvider.cs
+++ Northis.BattleRoostersOnline/Implements/GameServicesProvider.cs
@@ -1,25 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using CommonServiceLocator;
 using Northis.BattleRoostersOnline.Contracts;
-using Northis.BattleRoostersOnline.DataTransferObjects;
+using Northis.BattleRoostersOnline.DataStorages;
+using Unity;
+using Unity.ServiceLocation;
 
 namespace Northis.BattleRoostersOnline.Implements
 {
 	// ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "GameServicesProvider" в коде и файле конфигурации.
 	public class GameServicesProvider : IGameServicesProvider
 	{
-		public string GetData(int value)
+		public GameServicesProvider()
 		{
-			return string.Format("You entered: {0}", value);
+			var ioc = ServiceLocator.Current;
+			var container = new UnityContainer();
+			container.RegisterInstance(new ServicesStorage());
+
+			var locator = new UnityServiceLocator(container);
+			ServiceLocator.SetLocatorProvider(() => locator);
 		}
 
-		public CompositeType GetDataUsingDataContract(CompositeType composite)
-		{
-			
-		}
+		public string GetData(int value) => throw new NotImplementedException();
 	}
 }
