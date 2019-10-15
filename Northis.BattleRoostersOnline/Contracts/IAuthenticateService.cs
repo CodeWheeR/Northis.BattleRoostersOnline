1dec3e28eb57059eb0028aa8b746cc4d3402cc5a
Andrey Buturlakin <aa.buturlakin@northis.ru>
2019-10-15 14:11:08 +0300

    Добавлен IAuthenticateService интерфейс

---
+++ Northis.BattleRoostersOnline/Contracts/IAuthenticateService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+
+namespace Northis.BattleRoostersOnline.Contracts
+{
+	[ServiceContract]
+	interface IAuthenticateService
+	{
+		[OperationContract]
+		string LogIn(string login, string password);
+		[OperationContract]
+		string Register(string login, string password, string codePhrase);
+		[OperationContract(IsTerminating = true)]
+		bool LogOut();
+		[OperationContract]
+		string ResetPassword(string username, string codePhrase);
+	}
+}
