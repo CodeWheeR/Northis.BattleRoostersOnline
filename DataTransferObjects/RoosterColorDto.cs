6d4339ec986919dfd157e0a664dd027e8274f8e6
Сергей <se902i@gmail.com>
2019-10-15 14:11:02 +0300

    Добавлен проект Dto.

---
+++ DataTransferObjects/RoosterColorDto.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+	[DataContract]
+	public enum RoosterColorDto
+	{
+		[EnumMember]
+		Black,
+		[EnumMember]
+		Brown,
+		[EnumMember]
+		Blue,
+		[EnumMember]
+		Red,
+		[EnumMember]
+		White
+	}
+}
---
+++ DataTransferObjects/RoosterDto.cs
@@ -0,0 +1,94 @@
+﻿using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+	[DataContract]
+	public class RoosterDto
+    {
+		[DataMember]
+		public double Weight
+		{
+			get;
+			set;
+		}
+		[DataMember]
+		public int Height
+		{
+			get;
+			set;
+		}
+		[DataMember]
+		public double Health
+		{
+			get;
+			set;
+		}
+		[DataMember]
+		public int Stamina
+		{
+			get;
+			set;
+		}
+		[DataMember]
+		public RoosterColorDto ColorDto
+		{
+			get;
+			set;
+		}
+		[DataMember]
+		public int Brickness
+		{
+			get;
+			set;
+		}
+		[DataMember]
+		public CrestSizeDto Crest
+		{
+			get;
+			set;
+		}
+		[DataMember]
+		public int Thickness
+		{
+			get;
+			set;
+		}
+		[DataMember]
+		public int Luck
+		{
+			get;
+			set;
+		}
+		[DataMember]
+		public string Name
+		{
+			get;
+			set;
+		}
+		[DataMember]
+		public int WinStreak
+		{
+			get;
+			set;
+		}
+		[DataMember]
+		public double Hit
+		{
+			get;
+			set;
+		}
+		[DataMember]
+		public double Damage
+		{
+			get;
+			set;
+		}
+		[DataMember]
+		public int MahHealth
+		{
+			get;
+			set;
+		}
+
+	}
+}
