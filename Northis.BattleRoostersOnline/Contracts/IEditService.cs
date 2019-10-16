78b1d431cdb75ad8788fdf69e70190947c411921
Сергей <se902i@gmail.com>
2019-10-16 11:50:32 +0300

    Добавлены изменения Сергея

---
+++ Northis.BattleRoostersOnline/Contracts/IEditService.cs
@@ -0,0 +1,16 @@
+﻿using System.ServiceModel;
+using DataTransferObjects;
+
+namespace Northis.BattleRoostersOnline.Contracts
+{
+	[ServiceContract]
+	interface IEditService
+	{
+		[OperationContract]
+		void Edit(int userID, int roosterID, RoosterDto rooster);
+		[OperationContract]
+		void Add(int userID, RoosterDto rooster);
+		[OperationContract]
+		void Remove(int userID, int roosterID);
+	}
+}
---
+++ Northis.BattleRoostersOnline/Implements/EditService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+using DataTransferObjects;
+using Northis.BattleRoostersOnline.Contracts;
+
+namespace Northis.BattleRoostersOnline.Implements
+{
+	class EditService : IEditService
+	{
+		
+
+
+		public void Add(int userID, RoosterDto rooster)
+		{
+
+
+
+		}
+
+		public void Edit(int userID, int roosterID, RoosterDto rooster)
+		{
+			XDocument document = XDocument.Load("Resources/RoosterStorage.xml");
+			
+
+
+		}
+
+		public void Remove(int userID, int roosterID)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
