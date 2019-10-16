dd21a3bb0fc29543700b718edbb3c77ce37f6897
Сергей <se902i@gmail.com>
2019-10-16 15:49:48 +0300

    Добавлены коментарии.

--- Northis.BattleRoostersOnline/Contracts/IEditService.cs
+++ Northis.BattleRoostersOnline/Contracts/IEditService.cs
@@ -3,9 +3,18 @@ using DataTransferObjects;
 
 namespace Northis.BattleRoostersOnline.Contracts
 {
+	/// <summary>
+	/// Контракт, отвечающий за работу с петухами.
+	/// </summary>
 	[ServiceContract]
 	interface IEditService
 	{
+		/// <summary>
+		/// Редактирует выбранного петуха конкретного пользователя.
+		/// </summary>
+		/// <param name="userID">Идентификатор .</param>
+		/// <param name="roosterID">The rooster identifier.</param>
+		/// <param name="rooster">The rooster.</param>
 		[OperationContract]
 		void Edit(string userID, int roosterID, RoosterDto rooster);
 		[OperationContract]
