b1832a89b2d79e37c79667fe6541653277bc0cf3
Andrey Buturlakin <aa.buturlakin@northis.ru>
2019-10-28 16:17:46 +0300

    Реализовано отображение ограничителей в окне редактирования петухов.

---
+++ Northis.RoosterBattle/Converters/MaxDoubleValueToTextLimiter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using Catel.MVVM.Converters;
+using Northis.RoosterBattle.Extensions;
+
+namespace Northis.RoosterBattle.Converters
+{
+	/// <summary>
+	/// Конвертирует значение перечисления RoosterColor в путь к изображениям петухов.
+	/// </summary>
+	/// <seealso cref="Catel.MVVM.Converters.IValueConverter" />
+	public class MaxDoubleValue : IValueConverter
+	{
+		/// <summary>
+		/// Конвертирует значение Enum, помеченного аттрибутом Display, в путь к картинке.
+		/// </summary>
+		/// <param name="value">Значение перечисления RoosterColor.</param>
+		/// <param name="targetType">Целевой тип конвертации.</param>
+		/// <param name="parameter">Параметр.</param>
+		/// <param name="culture">Региональные настройки и параметры.</param>
+		/// <returns>Строковый путь к изображению.</returns>
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => $"(0.0 - {(int)value}.0)";
+
+		/// <summary>
+		/// Метод не имеет реализации.
+		/// </summary>
+		/// <param name="value">Значение перечисления RoosterColor.</param>
+		/// <param name="targetType">Целевой тип конвертации.</param>
+		/// <param name="parameter">Параметр.</param>
+		/// <param name="culture">Региональные настройки и параметры.</param>
+		/// <returns></returns>
+		/// <exception cref="NotImplementedException"></exception>
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+	}
+}
